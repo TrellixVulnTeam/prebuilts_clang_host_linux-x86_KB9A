01d46bec8a71c1f33344d4d16cde3be0bd7ef836
Carlos Solano <csolano@gmail.com>
2021-03-19 08:45:23 +0100

    clang: update to 12.0.4
    Change-Id: I177194111f21335304510c352cb86e63334e9dfc

--- clang_custom.mk
+++ clang_custom.mk
@@ -1,6 +1,6 @@
 #
 # define the latest known clang version
-CLANG_CUSTOM_VERSION_LATEST := r412851
+CLANG_CUSTOM_VERSION_LATEST := r416183
 
 # define the custom clang path
 TARGET_KERNEL_CLANG_PATH := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)_custom/clang-$(CLANG_CUSTOM_VERSION_LATEST)
